6b09fc052b5defdc97bf9c57da0636d688521a59
Damen <damendeberry@aol.com>
2023-06-01 02:58:33 -0400

    version: 2.0.0 initial
    - add: config sections
    - add: consume options (sound/area)
    - add: whitelist ids (sound/area)
    - add: blacklist ids (sound/area)
    - add: debug UI tool to review sound ids (sound/area/silent)
    - add: license snippets to files
    - cleanup: checkstyle
    - fix: security exception log for directory creation if permissions do not allow
    - update: plugin properties for plugin hub and version (2.0)

--- src/main/java/com/soundswapper/SoundSwapperConfig.java
+++ src/main/java/com/soundswapper/SoundSwapperConfig.java
@@ -1,37 +1,181 @@
+/*
+ * Copyright (c) 2023, petertalbanese <https://github.com/petertalbanese>
+ * Copyright (c) 2023, damencs <https://github.com/damencs>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package com.soundswapper;
 
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
+import net.runelite.client.config.ConfigSection;
 
 @ConfigGroup("soundswapper")
 public interface SoundSwapperConfig extends Config
 {
+	/**
+	 * Sections
+	 */
+	@ConfigSection(
+			name = "Sound Effects",
+			description = "Configuration settings for Sound Effects",
+			position = 1,
+			closedByDefault = true
+	)
+	String SOUND_EFFECTS_SECTION = "soundEffects";
+
+	@ConfigSection(
+			name = "Area Sound Effects",
+			description = "Configuration settings for Area Sound Effects",
+			position = 2,
+			closedByDefault = true
+	)
+	String AREA_SOUND_EFFECTS_SECTION = "areaSoundEffects";
+
+	/**
+	 * Config Items
+	 */
 	@ConfigItem(
-			keyName = "SoundEffects",
-			name = "Sounds Effects",
+			keyName = "soundEffects",
+			name = "Swap Sound Effects",
 			description = "Swap sound effects with custom sounds",
-			position = 0
+			position = 1,
+			section = SOUND_EFFECTS_SECTION
 	)
 	default boolean soundEffects() { return false; }
 
 	@ConfigItem(
-			keyName = "AreaSoundEffects",
-			name = "Area Sound Effects",
-			description = "Swap area sound effects with custom sounds",
-			position = 1
-	)
-	default boolean areaSoundEffects() { return false; }
-
-	@ConfigItem(
-			keyName = "CustomSounds",
+			keyName = "customSounds",
 			name = "Custom Sounds",
-			description = "Sounds to replace with your own custom .wav files. Separate with comma. Sound List: https://oldschool.runescape.wiki/w/List_of_in-game_sound_IDs",
-			position = 2
+			description = "Area Sounds to replace with your own custom .wav files. Separate with comma. Sound List: https://oldschool.runescape.wiki/w/List_of_in-game_sound_IDs",
+			position = 2,
+			section = SOUND_EFFECTS_SECTION
 	)
 	default String customSounds()
 	{
 		return "";
 	}
-}
 
+	@ConfigItem(
+			keyName = "consumeSoundEffects",
+			name = "Consume Sounds Effects",
+			description = "Consume any game sound effect that is not custom",
+			position = 3,
+			section = SOUND_EFFECTS_SECTION
+	)
+	default boolean consumeSoundEffects() { return false; }
+
+	@ConfigItem(
+			keyName = "whitelistSounds",
+			name = "Whitelist Sounds",
+			description = "Sound ids allowed to bypass the 'Consume Sound Effects' config option\n" +
+					"Format: 123,456,789",
+			position = 4,
+			section = SOUND_EFFECTS_SECTION
+	)
+	default String whitelistSounds()
+	{
+		return "";
+	}
+
+	@ConfigItem(
+			keyName = "blacklistedSounds",
+			name = "Blacklist Sounds",
+			description = "Sound ids consumed regardless of the 'Consume Sound Effects' config option being enabled\n" +
+					"Format: 123,456,789",
+			position = 5,
+			section = SOUND_EFFECTS_SECTION
+	)
+	default String blacklistedSounds()
+	{
+		return "";
+	}
+
+	@ConfigItem(
+			keyName = "areaSoundEffects",
+			name = "Swap Area Sounds",
+			description = "Swap area sound effects with custom sounds",
+			position = 1,
+			section = AREA_SOUND_EFFECTS_SECTION
+	)
+	default boolean areaSoundEffects() { return false; }
+
+	@ConfigItem(
+			keyName = "customAreaSounds",
+			name = "Custom Area Sounds",
+			description = "Area Sounds to replace with your own custom .wav files. Separate with comma. Sound List: https://oldschool.runescape.wiki/w/List_of_in-game_sound_IDs",
+			position = 2,
+			section = AREA_SOUND_EFFECTS_SECTION
+	)
+	default String customAreaSounds()
+	{
+		return "";
+	}
+
+	@ConfigItem(
+			keyName = "consumeAreaSounds",
+			name = "Consume Area Sounds",
+			description = "Consume any area sound effect that is not custom\n" +
+					"(Must have a custom sound file in the folder to allow this to work)",
+			position = 3,
+			section = AREA_SOUND_EFFECTS_SECTION
+	)
+	default boolean consumeAreaSounds() { return false; }
+
+	@ConfigItem(
+			keyName = "whitelistAreaSounds",
+			name = "Whitelist Area Sounds",
+			description = "Sound ids allowed to bypass the 'Consume Area Sound Effects' config option\n" +
+					"Format: 123,456,789",
+			position = 4,
+			section = AREA_SOUND_EFFECTS_SECTION
+	)
+	default String whitelistAreaSounds()
+	{
+		return "";
+	}
+
+	@ConfigItem(
+			keyName = "blacklistedAreaSounds",
+			name = "Blacklist Area Sounds",
+			description = "Sound ids consumed regardless of the 'Consume Area Sound Effects' config option being enabled\n" +
+					"Format: 123,456,789",
+			position = 5,
+			section = AREA_SOUND_EFFECTS_SECTION
+	)
+	default String blacklistedAreaSounds()
+	{
+		return "";
+	}
+
+	@ConfigItem(
+			keyName = "debugSoundEffects",
+			name = "Debug Sounds Effects",
+			description = "Display the sound effects that play (max 10 lines displayed)\n" +
+					"White: Sound Effect (G)\n" +
+					"Yellow: Area Sound Effect (A)\n" +
+					"Gray: Silent Area Sound Effect (SA)",
+			position = 99
+	)
+	default boolean debugSoundEffects() { return false; }
+}
--- src/main/java/com/soundswapper/SoundSwapperPlugin.java
+++ src/main/java/com/soundswapper/SoundSwapperPlugin.java
@@ -1,24 +1,51 @@
+/*
+ * Copyright (c) 2023, petertalbanese <https://github.com/petertalbanese>
+ * Copyright (c) 2023, damencs <https://github.com/damencs>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package com.soundswapper;
 
-import com.google.common.annotations.VisibleForTesting;
 import com.google.inject.Provides;
-import javax.inject.Inject;
-import javax.sound.sampled.*;
-
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.Client;
 import net.runelite.api.events.AreaSoundEffectPlayed;
 import net.runelite.api.events.SoundEffectPlayed;
 import net.runelite.client.RuneLite;
 import net.runelite.client.config.ConfigManager;
+import net.runelite.client.eventbus.EventBus;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.events.ConfigChanged;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.ui.overlay.OverlayManager;
 import net.runelite.client.util.Text;
 
+import javax.inject.Inject;
+import javax.sound.sampled.*;
 import java.io.*;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 @Slf4j
 @PluginDescriptor(
@@ -35,9 +62,24 @@ public class SoundSwapperPlugin extends Plugin
 	private Client client;
 
 	@Inject
+	private EventBus eventBus;
+
+	@Inject
 	private SoundSwapperConfig config;
 
-	private HashMap<Integer, Clip> clips = new HashMap<Integer, Clip>();
+	@Inject
+	private OverlayManager overlayManager;
+
+	@Inject
+	private SoundEffectOverlay soundEffectOverlay;
+
+	public HashMap<Integer, Clip> soundClips = new HashMap<>();
+	public HashMap<Integer, Clip> areaSoundClips = new HashMap<>();
+
+	public List<Integer> whitelistedSounds = new ArrayList<>();
+	public List<Integer> whitelistedAreaSounds = new ArrayList<>();
+	public List<Integer> blacklistedSounds = new ArrayList<>();
+	public List<Integer> blacklistedAreaSounds = new ArrayList<>();
 
 	private static final File SOUND_DIR = new File(RuneLite.RUNELITE_DIR, "SoundSwapper");
 
@@ -52,78 +94,186 @@ public class SoundSwapperPlugin extends Plugin
 	@Override
 	protected void startUp() throws Exception
 	{
-		SOUND_DIR.mkdir();
-		updateList();
+        try
+		{
+            if (!SOUND_DIR.exists())
+            {
+                SOUND_DIR.mkdir();
+            }
+        }
+		catch (SecurityException securityException)
+        {
+            log.error("Attempted to create SoundSwapper directory and a security exception prompted a fault");
+        }
+
+		updateLists();
+
+		overlayManager.add(soundEffectOverlay);
+		eventBus.register(soundEffectOverlay);
+	}
+
+	@Override
+	protected void shutDown() throws Exception
+	{
+		eventBus.unregister(soundEffectOverlay);
+		overlayManager.remove(soundEffectOverlay);
 	}
 
 	@Subscribe
 	public void onConfigChanged(ConfigChanged event)
 	{
-		if (!CONFIG_GROUP.equals(event.getGroup())) {
+		if (!CONFIG_GROUP.equals(event.getGroup()))
+		{
 			return;
 		}
 
-		updateList();
-	}
+		switch (event.getKey())
+		{
+			case "customSounds":
+			{
+				updateClipList(soundClips, event.getNewValue());
+			}
 
-	@VisibleForTesting
-	void updateList()
-	{
-		for (int sound : clips.keySet()) {
-			Clip clip = clips.get(sound);
-			if (clip != null) {
-				if (clip.isOpen()) {
-					clip.close();
-				}
+			case "customAreaSounds":
+			{
+				updateClipList(areaSoundClips, event.getNewValue());
+			}
+
+			case "whitelistOtherSounds":
+			{
+				whitelistedSounds = getIds(event.getNewValue());
+			}
+
+			case "whitelistAreaSounds":
+			{
+				whitelistedAreaSounds = getIds(event.getNewValue());
+			}
+
+			case "blacklistedSounds":
+			{
+				blacklistedSounds = getIds(event.getNewValue());
+			}
+
+			case "blacklistedAreaSounds":
+			{
+				blacklistedAreaSounds = getIds(event.getNewValue());
+			}
+
+			case "debugSoundEffects":
+			{
+				soundEffectOverlay.resetLines();
 			}
 		}
-		clips.clear();
-		for (String s : Text.fromCSV(config.customSounds())) {
-			try {
-				int id = Integer.parseInt(s);
-				tryLoadSound(s, id);
-			} catch (NumberFormatException e) {
-				log.warn("Invalid sound ID: {}", s);
-			}
+	}
+
+	void updateLists()
+	{
+		if (!config.customSounds().isEmpty())
+		{
+			updateClipList(soundClips, config.customSounds());
+		}
+
+		if (!config.customAreaSounds().isEmpty())
+		{
+			updateClipList(areaSoundClips, config.customAreaSounds());
+		}
+
+		if (!config.whitelistSounds().isEmpty())
+		{
+			whitelistedSounds = getIds(config.whitelistSounds());
+		}
+
+		if (!config.whitelistAreaSounds().isEmpty())
+		{
+			whitelistedAreaSounds = getIds(config.whitelistAreaSounds());
+		}
+
+		if (!config.blacklistedSounds().isEmpty())
+		{
+			blacklistedSounds = getIds(config.blacklistedSounds());
+		}
+
+		if (!config.blacklistedAreaSounds().isEmpty())
+		{
+			blacklistedAreaSounds = getIds(config.blacklistedAreaSounds());
 		}
 	}
 
 	@Subscribe
 	public void onSoundEffectPlayed(SoundEffectPlayed event)
 	{
-		if (config.soundEffects()) {
-			int eventSound = event.getSoundId();
-			if (clips.containsKey(eventSound)) {
+		int soundId = event.getSoundId();
+
+		if (config.soundEffects())
+		{
+			if (soundClips.containsKey(soundId))
+			{
 				event.consume();
-				playCustomSound(eventSound);
+				playCustomSound(soundClips.get(soundId));
+				return;
 			}
+		}
+
+		if (config.consumeSoundEffects() || blacklistedSounds.contains(soundId))
+		{
+			if (!whitelistedSounds.isEmpty() && whitelistedSounds.contains(soundId))
+			{
+				log.debug("whitelisted other sound effect passed: {}", soundId);
+				return;
+			}
+
+			log.debug("consumed other sound effect: {}", soundId);
+			event.consume();
 		}
 	}
 
 	@Subscribe
 	public void onAreaSoundEffectPlayed(AreaSoundEffectPlayed event)
 	{
-		if (config.areaSoundEffects()) {
-			int eventSound = event.getSoundId();
-			if (clips.containsKey(eventSound)) {
+		int soundId = event.getSoundId();
+
+		if (config.areaSoundEffects())
+		{
+			if (areaSoundClips.containsKey(soundId))
+			{
 				event.consume();
-				playCustomSound(eventSound);
+				playCustomSound(areaSoundClips.get(soundId));
+				return;
 			}
+		}
+
+		if (config.consumeAreaSounds() || blacklistedSounds.contains(soundId))
+		{
+			if (!whitelistedAreaSounds.isEmpty() && whitelistedAreaSounds.contains(soundId))
+			{
+				log.debug("whitelisted area sound effect passed: {}", soundId);
+				return;
+			}
+
+			log.debug("consumed area sound effect: {}", soundId);
+			event.consume();
 		}
 	}
 
-	private boolean tryLoadSound(String sound_name, Integer sound_id)
+	private boolean tryLoadSound(HashMap<Integer, Clip> map, String sound_name, Integer sound_id)
 	{
 		File sound_file = new File(SOUND_DIR, sound_name + ".wav");
-		if (sound_file.exists()) {
-			try {
+
+		if (sound_file.exists())
+		{
+			try
+			{
 				InputStream fileStream = new BufferedInputStream(new FileInputStream(sound_file));
 				AudioInputStream sound = AudioSystem.getAudioInputStream(fileStream);
+
 				Clip clip = AudioSystem.getClip();
 				clip.open(sound);
-				clips.put(sound_id, clip);
+				map.put(sound_id, clip);
+
 				return true;
-			} catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+			}
+			catch (UnsupportedAudioFileException | IOException | LineUnavailableException e)
+			{
 				log.warn("Unable to load custom sound " + sound_name, e);
 			}
 		}
@@ -131,10 +281,64 @@ public class SoundSwapperPlugin extends Plugin
 		return false;
 	}
 
-	private synchronized void playCustomSound(Integer sound_id)
+	private void updateClipList(HashMap<Integer, Clip> clips, String configText)
 	{
-		Clip clip = clips.get(sound_id);
-		if (clip == null) {
+		for (int sound : clips.keySet())
+		{
+			Clip clip = clips.get(sound);
+
+			if (clip != null)
+			{
+				if (clip.isOpen())
+				{
+					clip.close();
+				}
+			}
+		}
+
+		clips.clear();
+
+		for (String s : Text.fromCSV(configText))
+		{
+			try
+			{
+				int id = Integer.parseInt(s);
+				tryLoadSound(clips, s, id);
+			} catch (NumberFormatException e)
+			{
+				log.warn("Invalid sound ID: {}", s);
+			}
+		}
+	}
+
+	private List<Integer> getIds(String configText)
+	{
+		if (configText == null || configText.isEmpty())
+		{
+			return null;
+		}
+
+		List<Integer> ids = new ArrayList<>();
+		for (String s : Text.fromCSV(configText))
+		{
+			try
+			{
+				int id = Integer.parseInt(s);
+				ids.add(id);
+			}
+			catch (NumberFormatException e)
+			{
+				log.warn("Invalid id when parsing {}: {}", configText, s);
+			}
+		}
+
+		return ids;
+	}
+
+	private void playCustomSound(Clip clip)
+	{
+		if (clip == null)
+		{
 			return;
 		}
 
